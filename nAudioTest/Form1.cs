d2d09d7c2716c54d4bf306120e7f94ad1a6a6c3f
bbangjun360 <jysbruh0602@gmail.com>
2023-10-24 17:08:02 +0900

    Add project files.

---
+++ nAudioTest/Form1.cs
@@ -0,0 +1,32 @@
+﻿using NAudio.Wave;
+using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using NAudio.Dmo;
+using NAudio.Wave;
+using NAudio.Wave.SampleProviders;
+
+namespace nAudioTest
+{
+    public partial class Form1 : Form
+    {
+        Mp3Player PlayerSpk = new Mp3Player();
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            PlayerSpk.StopAndPlay(@"C:\Users\user\Desktop\bbangjun\HAS2_Train\wavPlayerNarr\SCN1.wav");
+        }
+    }
+}
---
+++ nAudioTest/MP3Player.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using NAudio.Dmo;
+using NAudio.Wave;
+using NAudio.Wave.SampleProviders;
+
+namespace nAudioTest
+{
+    public class Mp3Player
+    {
+        public WaveOutEvent outputDevice = new WaveOutEvent();
+        public AudioFileReader audioFile;
+        public int nDeviceNum = 0;
+        public int nDeviceVolume = 50;
+        public void PlayMp3(String filename)
+        {
+            try
+            {
+                if (outputDevice != null)
+                {
+                    outputDevice.Stop();
+                    Thread.Sleep(150);
+                }
+                if (outputDevice == null)
+                {
+                    outputDevice = new WaveOutEvent()
+                    {
+                        DeviceNumber = nDeviceNum
+                    };
+                    outputDevice.PlaybackStopped += OnPlaybackStopped;
+                }
+                if (audioFile == null)
+                {
+                    //String strTemp = "C:\\Users\\user\\Downloads\\" + filename + ".wav";
+                    Console.WriteLine(filename);
+                    audioFile = new AudioFileReader(@filename);
+                    outputDevice.Init(audioFile);
+                    audioFile.Volume = nDeviceVolume / 100f;
+                    outputDevice.Play();
+                    while (outputDevice.PlaybackState == PlaybackState.Playing)
+                    {
+                        System.Threading.Thread.Sleep(100);
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"An error occured while trying to stop the MP3: {e.Message}");
+            }
+        }
+        public void StopAndPlay(String filename)
+        {
+            if (outputDevice.PlaybackState == PlaybackState.Playing)
+            {
+                outputDevice.Stop();
+                outputDevice.Dispose();
+                outputDevice = new WaveOutEvent();
+            }
+                PlayAudio(filename);
+        }
+        private void PlayAudio(string filePath)
+        {
+            if (File.Exists(filePath))
+            {
+                Console.WriteLine($"Playing: {Path.GetFileName(filePath)}");
+                using (var waveFileReader = new WaveFileReader(filePath))
+                {
+                    outputDevice.Init(waveFileReader);
+                    outputDevice.Play();
+                    while (outputDevice.PlaybackState == PlaybackState.Playing)
+                    {
+                        Application.DoEvents();
+                        System.Threading.Thread.Sleep(100);
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("File not found.");
+            }
+        }
+        public void StopMp3()
+        {
+            outputDevice.Stop();
+        }
+        private void OnPlaybackStopped(object sender, StoppedEventArgs args)
+        {
+            try
+            {
+                outputDevice.Dispose();
+                outputDevice = null;
+                audioFile.Dispose();
+                audioFile = null;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"An error occured while trying to stop the MP3: {e.Message}");
+            }
+        }
+    }
+}
